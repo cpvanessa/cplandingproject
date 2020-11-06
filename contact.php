55180a905fabfd6027c1bdbfb96c6a971f714d81
cpvanessa <63360171+cpvanessa@users.noreply.github.com>
2020-11-06 23:16:39 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projet</title>
+</head>
+<body>
+
+<?php
+echo 'Merci! <br/>';
+echo 'Ton email est ',$_POST['email'], '<br/>';
+echo 'Ton message est ', $_POST['comment'];
+
+
+
+
+?>
+</body>
+</html>
